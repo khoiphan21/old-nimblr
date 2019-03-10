eaf8ad8ce1bce7e51489fa69134efd1160f41d5d
Khoi Phan <khoiphan21@gmail.com>
2019-03-10 22:52:26 +1000

    all tests are faster than 1s now

--- src/app/pages/dashboard-page/dashboard-page.component.spec.ts
+++ src/app/pages/dashboard-page/dashboard-page.component.spec.ts
@@ -1,15 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardPageComponent } from './dashboard-page.component';
-import { HeaderComponent } from '../../components/header/header.component';
-import { DocumentCardComponent } from '../../components/document-card/document-card.component';
-import { ServicesModule } from '../../modules/services.module';
 import { RouterTestingModule } from '@angular/router/testing';
 import { Router } from '@angular/router';
 import { DocumentService } from 'src/app/services/document/document.service';
 import { Subject } from 'rxjs';
-import { AccountService } from '../../services/account/account.service';
-import { MockAccountService } from '../../services/account/account-impl.service.spec';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 class MockDocumentService {
   getUserDocuments$() {
@@ -25,24 +21,18 @@ describe('DashboardPageComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        DashboardPageComponent,
-        HeaderComponent,
-        DocumentCardComponent
+        DashboardPageComponent
       ],
       imports: [
-        ServicesModule,
         RouterTestingModule.withRoutes([])
       ],
       providers: [
         {
           provide: DocumentService,
           useClass: MockDocumentService
-        },
-        {
-          provide: AccountService,
-          useClass: MockAccountService
         }
-      ]
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
     .compileComponents();
 
