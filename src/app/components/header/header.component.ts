ef4e6ecbb65cc6be6cd664ee70630c118a1bbe9d
Jeremy Yung Hau Ng <jeremy.ng1002@gmail.com>
2019-03-12 11:31:19 +1000

    create interface for mobile document options

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { User } from '../../classes/user';
 import { AccountService } from '../../services/account/account.service';
+import { Router } from '@angular/router';
+import { NavigationBarService } from 'src/app/services/navigation-bar/navigation-bar.service';
 
 @Component({
   selector: 'app-header',
@@ -8,16 +10,41 @@ import { AccountService } from '../../services/account/account.service';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  currentUrl: string;
+  initialName: string;
+  isOptionShown = false;
   @Input() currentUser: User;
 
   constructor(
-    private accountService: AccountService
+    private navigationBarService: NavigationBarService,
+    private accountService: AccountService,
+    private router: Router
   ) { }
 
   ngOnInit() {
     this.accountService.getUser$().subscribe((user) => {
-      this.currentUser = user;
+      if (user !== null) {
+        this.currentUser = user;
+        const firstName = user.firstName;
+        this.processInitialName(firstName);
+      }
     });
+    this.manageHeaderContent(this.router.url);
   }
 
+  processInitialName(fName: string) {
+    this.initialName = fName.charAt(0);
+  }
+
+  manageHeaderContent(url: string) {
+    if (url === '/dashboard') {
+      this.currentUrl = 'dashboard';
+    } else {
+      this.currentUrl = 'document';
+    }
+  }
+
+  showNavigationBar() {
+      this.navigationBarService.setNavigationBarStatus(true);
+  }
 }
