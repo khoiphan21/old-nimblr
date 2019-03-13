c4aec0b3cb06a2ddfcd91d761195351b7d92463c
brunocheung <brunocyh@gmail.com>
2019-03-13 17:32:27 +1000

    test send query in correct order: still failed

--- src/app/services/graphQL/graph-ql-command.service.spec.ts
+++ src/app/services/graphQL/graph-ql-command.service.spec.ts
@@ -27,9 +27,20 @@ describe('GraphQlCommandService', () => {
 
     });
 
+    it('should remove query from queue when API response', () => {
+
+    });
+
+    it('should clear out the queue eventually if queries are processed successfully', () => {
+
+    });
+
+    it('should resend the same query after timeout if there is no response from cloud API', () => {
+
+    });
   });
 
-  fdescribe('Unit Tests', () => {
+  fdescribe('Unit Tests: Happy Path', () => {
     const TEST_QUERY = '';
     const TEST_PARAMETERS = '';
 
@@ -41,59 +52,70 @@ describe('GraphQlCommandService', () => {
       graphQlService = TestBed.get(GraphQlCommandService);
     });
 
-    async function mockQuery(): Promise<any> {
-      return graphQlService.query(TEST_QUERY, TEST_PARAMETERS);
-    };
+    // async function mockQuery(): Promise<any> {
+    //   return graphQlService.query(TEST_QUERY, TEST_PARAMETERS);
+    // };
 
     it('should return a Promise type when query is registered correctly', async () => {
-      const response = mockQuery();
+      const response = graphQlService.query(TEST_QUERY, TEST_PARAMETERS);
       expect(response instanceof Promise).toBeTruthy();
     });
 
-    // it('should return a Promise type when error', () => {
-    //   const value = graphQlService.query(TEST_QUERY, TEST_PARAMETERS);
-    // });
 
-    fit('should call queue add method when new query comes in', () => {
+    it('should add in corresponding query into the queue', () => {
       // scope: test whether the promise is passed into the queue but using add
       // out scope: not the actual behaviour of the external queue service
-      
+
       // REF: https://www.npmjs.com/package/p-queue
       // Set up and mock the queue
       spyOn(graphQlService['queryQueue'], 'add');
       expect(graphQlService['queryQueue'].add.calls.count()).toBe(0);
-      graphQlService.query(TEST_QUERY, TEST_PARAMETERS);
-      expect(graphQlService['queryQueue'].add.calls.count()).toBe(1);
+
+      // execute mock query 10 times and check call counts
+      for (var c = 1; c <= 10; c++) {
+        graphQlService.query(TEST_QUERY, TEST_PARAMETERS);
+        expect(graphQlService['queryQueue'].add.calls.count()).toBe(c);
+      }
+    });
+
+    fit('should send queries to queue in chronological order according to its enqueued time', () => {
+      // Set up and mock the queue
+      spyOn(graphQlService['queryQueue'], 'add');
+
+      // execute mock query and check whether queries are added in correct order
+      // TODO: NO, not completed yet, the parameter have been chuck into add is a function
+      // not numbers
+      for (var c = 1; c <= 10; c++) {
+        let mockParameter = c.toString();
+        console.log(typeof(mockParameter));
+        graphQlService.query(mockParameter, mockParameter);
+        expect(graphQlService['queryQueue'].add).toHaveBeenCalledWith(mockParameter, mockParameter);
+      }
 
     });
 
-    it('should resend the same query after timeout if there is no response from cloud API', () => {
+    it('should resolve queries in FIFO order', () => {
 
     });
 
-    it('should stores all queries in one task queue only', () => {
+  });
 
+  describe('Unit Tests: Error Path', () => {
+    const TEST_QUERY = '';
+    const TEST_PARAMETERS = '';
+
+    let graphQlService: GraphQlCommandService;
+    TestBed.configureTestingModule({});
+
+    beforeEach(() => {
+      spyOn(API, 'graphql').and.returnValue(Promise.resolve());
+      graphQlService = TestBed.get(GraphQlCommandService);
     });
 
-    it('should perform queries in FIFO order', () => {
-
+    it('should return a Promise type when error', () => {
+      // const value = graphQlService.query(TEST_QUERY, TEST_PARAMETERS);
     });
 
-    it('should store all queries in chronological order according to its registered time', () => {
-
-    });
-
-    it('should remove query from queue when API response', () => {
-
-    });
-
-    it('should resolve the corresponding Promise after API response and been removed from queue', () => {
-
-    });
-
-    it('should clear out the queue eventually if queries are processed successfully', () => {
-
-    });
   });
 
 });
