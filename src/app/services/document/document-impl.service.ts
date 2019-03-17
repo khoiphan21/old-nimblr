06c83ee2e403b4280c04dedcb9fc8bafbdcab68e
Khoi Phan <khoiphan21@gmail.com>
2019-03-18 08:08:41 +1000

    complete full coverage for DocumentService

--- src/app/services/document/document-impl.service.ts
+++ src/app/services/document/document-impl.service.ts
@@ -6,12 +6,13 @@ import { AccountService } from '../account/account.service';
 import { GraphQLService } from '../graphQL/graph-ql.service';
 import { User } from '../../classes/user';
 import { API, graphqlOperation } from 'aws-amplify';
-import * as mutations from '../../../graphql/mutations';
-import * as queries from '../../../graphql/queries';
-import * as subscriptions from '../../../graphql/subscriptions';
 import { CreateDocumentInput, DocumentType } from '../../../API';
 import { Deferred } from '../../classes/deferred';
 import { DocumentFactoryService } from './factory/document-factory.service';
+import { listDocuments } from 'src/graphql/queries';
+import { createDocument, deleteDocument } from 'src/graphql/mutations';
+import { onCreateDocument } from 'src/graphql/subscriptions';
+import { GraphQLError } from '../graphQL/error';
 
 
 @Injectable({
@@ -19,7 +20,7 @@ import { DocumentFactoryService } from './factory/document-factory.service';
 })
 export class DocumentServiceImpl implements DocumentService {
 
-  private currentDocument$: BehaviorSubject<Document> = new BehaviorSubject(null);
+  private currentDocument$: BehaviorSubject<Document>;
   private userDocuments$: BehaviorSubject<Array<Document>>;
 
   constructor(
@@ -27,12 +28,19 @@ export class DocumentServiceImpl implements DocumentService {
     private documentFactory: DocumentFactoryService,
     private graphQlService: GraphQLService
   ) {
+    this.currentDocument$ = new BehaviorSubject(null);
   }
 
+  /**
+   * Retrieve the list of documents for a certain user
+   *
+   * @param userId the id of the user to retrieve documents
+   */
   private async getDocumentsForUserId(userId: string): Promise<Array<Document>> {
-    const deferred: Deferred<Array<Document>> = new Deferred();
+    let response: any; // response from the backend
 
-    const queryParams = {
+    // setup the arguments for the query
+    const queryArgs = {
       filter: {
         ownerId: {
           eq: userId
@@ -40,54 +48,73 @@ export class DocumentServiceImpl implements DocumentService {
       }
     };
 
-    const response: any = await this.graphQlService.query(
-      queries.listDocuments, queryParams
+    // Retrieve the list of raw documents from the backend
+    try {
+      response = await this.graphQlService.query(
+        listDocuments, queryArgs
+      );
+    } catch (error) {
+      throw new GraphQLError({
+        message: `failed to retrieve documents for user ${userId}`,
+        query: listDocuments,
+        params: queryArgs,
+        backendResponse: error
+      });
+    }
+    const documents: Array<any> = response.data.listDocuments.items;
+
+    // Convert the raw documents into Document objects
+    const parsedDocuments: Array<Document> = documents.map(
+      (rawDocument: any) => {
+        return this.documentFactory.createDocument(rawDocument);
+      }
     );
 
-    const documentPromisses: Array<Promise<Document>> = response.data.listDocuments.items.map(rawDocument => {
-      return this.documentFactory.createDocument(rawDocument);
-    });
-
-    Promise.all(documentPromisses).then((documents: Array<Document>) => {
-      deferred.resolve(documents);
-    });
-
-    return deferred.promise;
+    return parsedDocuments;
   }
 
+  /**
+   * Create a new form document and set the current document to
+   * be that newly created document
+   */
   async createFormDocument(): Promise<Document> {
-    const deferred: Deferred<Document> = new Deferred();
+    let response: any; // the response from GraphQL query
 
-    try {
-      const user = await this.accountService.isUserReady();
+    const user = await this.accountService.isUserReady();
 
-      const documentDetails = {
+    const queryArg = {
+      input: {
         type: DocumentType.FORM,
         title: null,
         ownerId: user.id,
         editorIds: [],
         viewerIds: [],
-        order: [],
-      } as CreateDocumentInput;
+        order: []
+      }
+    };
 
-      const response: any = await API.graphql(
-        graphqlOperation(mutations.createDocument, { input: documentDetails })
+    try {
+      response = await this.graphQlService.query(
+        createDocument, queryArg
       );
-
-      const rawDocument = response.data.createDocument;
-
-      // Create a document, then emit the newly created document
-      const document = this.documentFactory.createDocument(rawDocument);
-      
-      this.currentDocument$.next(document);
-      deferred.resolve(document);
     } catch (error) {
-      console.error('error received in createFormDocument()');
-      deferred.reject(error);
+      throw new GraphQLError({
+        message: `failed to create form document`,
+        query: createDocument,
+        params: queryArg,
+        backendResponse: error
+      });
     }
 
+    const rawDocument = response.data.createDocument;
 
-    return deferred.promise;
+    // Create a document, then emit the newly created document
+    const document = this.documentFactory.createDocument(rawDocument);
+
+    this.currentDocument$.next(document);
+
+
+    return document;
   }
 
   getCurrentDocument$(): Observable<Document> {
@@ -104,47 +131,40 @@ export class DocumentServiceImpl implements DocumentService {
             this.userDocuments$.next(documents);
           }
         );
-      }).catch(error => {
-        console.error('Error in getUserDocuments$');
-        this.userDocuments$.error(error);
+      }).catch(() => {
+        const message = 'Error getting documents: User is not logged in';
+        this.userDocuments$.error(message);
       });
     }
     return this.userDocuments$;
   }
 
-  private async setupSubscriptionForUserDocuments() {
+  private async setupSubscriptionForUserDocuments(): Promise<any> {
+    let user: User;
+    // Check if the user is logged in
     try {
-      const user: User = await this.accountService.isUserReady();
-      const subscription: any = API.graphql(
-        graphqlOperation(subscriptions.onCreateDocument)
-      );
-      subscription.subscribe(response => {
-        this.getDocumentsForUserId(user.id).then(
-          documents => {
-            this.userDocuments$.next(documents);
-          }
-        );
-      });
-    } catch (error) {
-      console.error('Error setting up subscription for user documents. More details below.');
-      console.error(error);
+      user = await this.accountService.isUserReady();
+    } catch {
+      throw new Error('Unable to setup subscription for user documents');
     }
+    this.graphQlService.getSubscription(onCreateDocument).subscribe(() => {
+      this.getDocumentsForUserId(user.id).then(
+        documents => {
+          this.userDocuments$.next(documents);
+        }
+      );
+    }, () => { // error pathway
+      const message = `Unable to setup subscription for user documents: failed to call graphQl`;
+      this.userDocuments$.error(message);
+    });
   }
 
   async deleteDocument(id: string): Promise<any> {
-    try {
-      const params = {
-        input: {
-          id: id
-        }
-      };
-      const deletedDocument = await API.graphql(
-        graphqlOperation(mutations.deleteDocument, params)
-      );
-      return Promise.resolve(deletedDocument);
-    } catch (error) {
-      return Promise.reject(error);
-    }
+    return this.graphQlService.query(deleteDocument, { input: { id } })
+      .catch(error => {
+        const message = `Unable to delete document: ${error.message}`;
+        return Promise.reject(Error(message));
+      });
   }
 
 }
