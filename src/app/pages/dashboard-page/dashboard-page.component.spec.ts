5b90b5b08560644fe18fc2eddef34b33a76612b4
Jeremy Yung Hau Ng <jeremy.ng1002@gmail.com>
2019-03-18 13:28:59 +1000

    improve code coverage for dashboard page

--- src/app/pages/dashboard-page/dashboard-page.component.spec.ts
+++ src/app/pages/dashboard-page/dashboard-page.component.spec.ts
@@ -2,22 +2,25 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardPageComponent } from './dashboard-page.component';
 import { RouterTestingModule } from '@angular/router/testing';
-import { Router } from '@angular/router';
 import { DocumentService } from 'src/app/services/document/document.service';
-import { Subject } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { DocumentFactoryService } from 'src/app/services/document/factory/document-factory.service';
+const uuidv4 = require('uuid/v4');
 
 class MockDocumentService {
   getUserDocuments$() {
-    return new Subject();
+    return new BehaviorSubject(false);
   }
-  createFormDocument() {}
+  createFormDocument() { }
 }
 
-xdescribe('DashboardPageComponent', () => {
+fdescribe('DashboardPageComponent', () => {
   let component: DashboardPageComponent;
   let fixture: ComponentFixture<DashboardPageComponent>;
+  let documentFactory: DocumentFactoryService;
   let documentService;
+  let document;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -34,13 +37,18 @@ xdescribe('DashboardPageComponent', () => {
       ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(DashboardPageComponent);
     component = fixture.componentInstance;
     documentService = TestBed.get(DocumentService);
+    documentFactory = TestBed.get(DocumentFactoryService);
+    document = documentFactory.createDocument({
+      id: uuidv4(),
+      ownerId: uuidv4()
+    });
     fixture.detectChanges();
   });
 
@@ -48,17 +56,30 @@ xdescribe('DashboardPageComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it('should receive get the right value when the data comes in', () => {
+    spyOn(documentService, 'getUserDocuments$').and.callFake(() => {
+      return new BehaviorSubject([document]);
+    });
+    component.ngOnInit();
+    expect(component.userDocuments).toEqual([document]);
+  });
+
+  /* tslint:disable:no-string-literal */
   describe('createNewFormDocument()', () => {
+    let routerSpy;
+    beforeEach(() => {
+      routerSpy = spyOn(component['router'], 'navigate');
+    });
+
     it('should call createFormDocument() from Document Service', () => {
       spyOn(documentService, 'createFormDocument');
       component.createNewFormDocument();
       expect(documentService.createFormDocument).toHaveBeenCalled();
     });
 
-    xit('should navigate to "document" page when the function is called', () => {
+    it('should navigate to "document" page when the function is called', () => {
       component.createNewFormDocument();
-      // const routerSpy = spyOn(component['router'], 'navigate');
-      // expect(routerSpy.calls.count()).toBe(1);
+      expect(routerSpy.calls.count()).toBe(1);
     });
   });
 });
