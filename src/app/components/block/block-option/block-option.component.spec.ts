09b15e885081ccefbf4431e636d801541ecada6e
Jeremy Yung Hau Ng <jeremy.ng1002@gmail.com>
2019-03-19 10:56:03 +1000

    improve code coverage for block option

--- src/app/components/block/block-option/block-option.component.spec.ts
+++ src/app/components/block/block-option/block-option.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BlockOptionComponent } from './block-option.component';
 
-describe('BlockOptionComponent', () => {
+fdescribe('BlockOptionComponent', () => {
   let component: BlockOptionComponent;
   let fixture: ComponentFixture<BlockOptionComponent>;
 
@@ -16,10 +16,106 @@ describe('BlockOptionComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BlockOptionComponent);
     component = fixture.componentInstance;
+    spyOn<any>(component, 'toggleSelectedOptionsStatus');
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('ngOnChanges() -', () => {
+    beforeEach(() => {
+      component.ngOnChanges();
+    });
+
+    it('should change `isAddBlockContainerShown` to false', () => {
+      expect(component.isAddBlockContainerShown).toBe(false);
+    });
+
+    it('should change `isMenuSelectionContainerShown` to false', () => {
+      expect(component.isMenuSelectionContainerShown).toBe(false);
+    });
+  });
+
+  describe('showAddBlockContainer() -', () => {
+    beforeEach(() => {
+      component.showAddBlockContainer();
+    });
+
+    it('should change `isAddBlockContainerShown` to true', () => {
+      expect(component.isAddBlockContainerShown).toBe(true);
+    });
+
+    /* tslint:disable:no-string-literal */
+    it('should call toggleSelectedOptionsStatus() with the right value', () => {
+      expect(component['toggleSelectedOptionsStatus']).toHaveBeenCalledWith(true);
+    });
+  });
+
+  describe('hideAddBlockContainer() -', () => {
+    beforeEach(() => {
+      component.isAddBlockContainerShown = true;
+      component.hideAddBlockContainer();
+    });
+
+    it('should change `isAddBlockContainerShown` to false', () => {
+      expect(component.isAddBlockContainerShown).toBe(false);
+    });
+
+    /* tslint:disable:no-string-literal */
+    it('should call toggleSelectedOptionsStatus() with the right value', () => {
+      expect(component['toggleSelectedOptionsStatus']).toHaveBeenCalledWith(false);
+    });
+
+    it('should emit the right value to parent', () => {
+      component.switchBlockOptionsOff.subscribe((value) => {
+        expect(value).toEqual(false);
+      });
+    });
+  });
+
+  describe('showMenuSelectionContainer() -', () => {
+    beforeEach(() => {
+      component.showMenuSelectionContainer();
+    });
+
+    it('should change `isMenuSelectionContainerShown` to true', () => {
+      expect(component.isMenuSelectionContainerShown).toBe(true);
+    });
+
+    /* tslint:disable:no-string-literal */
+    it('should call toggleSelectedOptionsStatus() with the right value', () => {
+      expect(component['toggleSelectedOptionsStatus']).toHaveBeenCalledWith(true);
+    });
+  });
+
+  describe('hideMenuSelectionContainer() -', () => {
+    beforeEach(() => {
+      component.isMenuSelectionContainerShown = true;
+      component.hideMenuSelectionContainer();
+    });
+
+    it('should change `isMenuSelectionContainerShown` to false', () => {
+      expect(component.isMenuSelectionContainerShown).toBe(false);
+    });
+
+    /* tslint:disable:no-string-literal */
+    it('should call toggleSelectedOptionsStatus() with the right value', () => {
+      expect(component['toggleSelectedOptionsStatus']).toHaveBeenCalledWith(false);
+    });
+
+    it('should emit the right value to parent', () => {
+      component.switchBlockOptionsOff.subscribe((value) => {
+        expect(value).toEqual(false);
+      });
+    });
+  });
+
+  it('toggleSelectedOptionsStatus() - should emit the right value to parent', () => {
+    component.isSelectedOptionShown.subscribe((value) => {
+      expect(value).toEqual(false);
+    });
+    component['toggleSelectedOptionsStatus'](false);
   });
 });
