0247ebd364c2506443dd75ee0db6dc5749c9e068
Jeremy Yung Hau Ng <jeremy.ng1002@gmail.com>
2019-03-19 14:51:26 +1000

    improve code coverage for block component

--- src/app/components/block/block.component.spec.ts
+++ src/app/components/block/block.component.spec.ts
@@ -5,11 +5,25 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { BlockQueryService } from '../../services/block/query/block-query.service';
 import { MockBlockQueryService } from 'src/app/services/block/query/block-query.service.spec';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { BlockFactoryService } from 'src/app/services/block/factory/block-factory.service';
+import { BlockType } from 'src/API';
+const uuidv4 = require('uuid/v4');
 
 describe('BlockComponent', () => {
   let component: BlockComponent;
   let fixture: ComponentFixture<BlockComponent>;
-
+  let blockFactoryService: BlockFactoryService;
+  const rawData = {
+    id: uuidv4(),
+    type: BlockType.TEXT,
+    version: uuidv4(),
+    documentId: uuidv4(),
+    lastUpdatedBy: uuidv4(),
+    value: 'test',
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+  };
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -32,11 +46,39 @@ describe('BlockComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(BlockComponent);
+    blockFactoryService = TestBed.get(BlockFactoryService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  /* tslint:disable:no-string-literal */
+  describe('ngOnInit()', () => {
+    let block;
+    beforeEach(() => {
+      block = blockFactoryService.createAppBlock(rawData);
+    });
+
+    it('should set the block into the given value if it is not empty', () => {
+      spyOn<any>(component['blockQueryService'], 'getBlock$').and.callFake(() => {
+        return new BehaviorSubject(block);
+      });
+      component.ngOnInit();
+      expect(component.block).toEqual(block);
+    });
+  });
+
+  it('toggleBlockOptions() - should set `isBlockOptionsShown` to the given value', () => {
+    component.isSelectedOptionShown = false;
+    component.toggleBlockOptions(true);
+    expect(component.isBlockOptionsShown).toBe(true);
+  });
+
+  it('toggleSelectedOptionStatus() - should set `isSelectedOptionShown` to the given value', () => {
+    component.toggleSelectedOptionStatus(true);
+    expect(component.isSelectedOptionShown).toBe(true);
   });
 });
