6d2ec20ec5b4253f85f5632ee6ac9a8fcae7b723
Jeremy Yung Hau Ng <jeremy.ng1002@gmail.com>
2019-03-24 21:52:10 +1000

    create question block type

--- src/app/classes/question-block.ts
+++ src/app/classes/question-block.ts
@@ -1,16 +1,7 @@
-import { Block } from './block';
+import { Block, BlockCreateError } from './block';
 import { UUID, ISOTimeString } from '../services/document/command/document-command.service';
-import { BlockType } from 'src/API';
-
-export enum QuestionType {
-    PARAGRAPH = 'PARAGRAPH',
-    SHORT_ANSWER = 'SHORT_ANSWER',
-    CHECKBOX = 'CHECKBOX',
-    DROPDOWN = 'DROPDOWN',
-    MULTIPLE_CHOICE = 'MULTIPLE_CHOICE',
-}
-
-export interface QuestionBlock extends Block {
+import { BlockType, QuestionType } from '../../API';
+export class QuestionBlock implements Block {
     readonly id: UUID;
     readonly version: UUID;
     readonly type: BlockType;
@@ -18,42 +9,97 @@ export interface QuestionBlock extends Block {
     readonly lastUpdatedBy: UUID;
     readonly createdAt: ISOTimeString;
     readonly updatedAt: ISOTimeString;
-    readonly questionType: QuestionType;
     readonly question: string;
-}
-
-export class CheckboxQuestionBlock implements QuestionBlock {
-    readonly id: UUID;
-    readonly version: UUID;
-    readonly type: BlockType;
-    readonly documentId: UUID;
-    readonly lastUpdatedBy: UUID;
-    readonly createdAt: ISOTimeString;
-    readonly updatedAt: ISOTimeString;
     readonly questionType: QuestionType;
-    readonly question: string;
-    readonly options: [];
+    readonly options: Array<object>;
 
     constructor({
-      id,
-      version,
-      documentId,
-      lastUpdatedBy,
-      updatedAt,
-      createdAt,
-      questionType,
-      question,
-      options
+        id,
+        version,
+        type,
+        documentId,
+        lastUpdatedBy,
+        createdAt,
+        updatedAt,
+        question,
+        questionType,
+        options
     }) {
-      this.id = id;
-      this.version = version;
-      this.documentId = documentId;
-      this.type = BlockType.TEXT;
-      this.lastUpdatedBy = lastUpdatedBy;
-      this.createdAt = createdAt;
-      this.updatedAt = updatedAt;
-      this.questionType = questionType;
-      this.question = question;
-      this.options = options;
+        this.id = id;
+        this.version = version;
+        this.type = type;
+        this.documentId = documentId;
+        this.lastUpdatedBy = lastUpdatedBy;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.question = question;
+        this.questionType = questionType;
+        if (options) {
+            this.options = options;
+        } else {
+            this.options = this.generateOptionsForQuestionType(questionType);
+        }
     }
-  }
+
+    private generateOptionsForQuestionType(questionType: QuestionType): Array<QuestionOption> {
+        switch (questionType) {
+            case QuestionType.PARAGRAPH:
+                return [new ParagraphOption('')];
+            case QuestionType.SHORT_ANSWER:
+                return [new ShortAnswerOption('')];
+            case QuestionType.MULTIPLE_CHOICE:
+                return [new MultiplceChoiceOption('', '')];
+            case QuestionType.CHECKBOX:
+                return [new CheckBoxOption('', false)];
+            default:
+                throw new BlockCreateError(null, 'QuestionType not supported');
+        }
+    }
+}
+
+
+export interface QuestionOption {
+    answer: string;
+}
+
+export class ParagraphOption implements QuestionOption {
+    answer: string;
+    constructor(
+        answer: string,
+    ) {
+        this.answer = answer;
+    }
+}
+
+export class ShortAnswerOption implements QuestionOption {
+    answer: string;
+    constructor(
+        answer: string,
+    ) {
+        this.answer = answer;
+    }
+}
+
+export class CheckBoxOption implements QuestionOption {
+    checked: boolean;
+    answer: string;
+    constructor(
+        answer: string,
+        checked: boolean
+    ) {
+        this.answer = answer;
+        this.checked = checked;
+    }
+}
+
+export class MultiplceChoiceOption implements QuestionOption {
+    selected: string;
+    answer: string;
+    constructor(
+        answer: string,
+        selected: string
+    ) {
+        this.answer = answer;
+        this.answer = selected;
+    }
+}
