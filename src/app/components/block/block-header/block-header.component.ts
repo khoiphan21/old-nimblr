ce87cbefb039d86e6ca93642b0238b3c2532a836
brunocheung <brunocyh@gmail.com>
2019-03-25 16:02:49 +1000

    initial commit

--- src/app/components/block/block-header/block-header.component.ts
+++ src/app/components/block/block-header/block-header.component.ts
@@ -12,44 +12,44 @@ export class BlockHeaderComponent implements OnInit {
 
   block: any;
 
-  isPlaceholderShown: boolean;
-  value: string;
+  // isPlaceholderShown: boolean;
+  // value: string;
   private timeout: any;
 
   constructor(
-    private blockCommandService: BlockCommandService,
-    private factoryService: BlockFactoryService
+    // private blockCommandService: BlockCommandService,
+    // private factoryService: BlockFactoryService
   ) { }
 
   ngOnInit() {
-    this.value = this.block.value === null ? '' : this.block.value;
+    // this.value = this.block.value === null ? '' : this.block.value;
   }
 
-  async updateValue(): Promise<Block> {
-    return new Promise(resolve => {
-      clearTimeout(this.timeout);
-      this.timeout = setTimeout(() => {
-        const updatedBlock: Block = this.factoryService.createAppBlock({
-          id: this.block.id,
-          type: this.block.type,
-          documentId: this.block.documentId,
-          lastUpdatedBy: this.block.lastUpdatedBy,
-          value: this.value,
-          createdAt: this.block.createdAt
-        });
-        this.blockCommandService.updateBlock(updatedBlock).then(() => {
-          resolve(updatedBlock);
-        });
-      }, 500);
-    });
-  }
+  // async updateValue(): Promise<Block> {
+  //   return new Promise(resolve => {
+  //     clearTimeout(this.timeout);
+  //     this.timeout = setTimeout(() => {
+  //       const updatedBlock: Block = this.factoryService.createAppBlock({
+  //         id: this.block.id,
+  //         type: this.block.type,
+  //         documentId: this.block.documentId,
+  //         lastUpdatedBy: this.block.lastUpdatedBy,
+  //         value: this.value,
+  //         createdAt: this.block.createdAt
+  //       });
+  //       this.blockCommandService.updateBlock(updatedBlock).then(() => {
+  //         resolve(updatedBlock);
+  //       });
+  //     }, 500);
+  //   });
+  // }
 
-  togglePlaceholder(status: boolean) {
-    if (this.value.length > 0 || status === false) {
-      this.isPlaceholderShown = false;
-    } else {
-      this.isPlaceholderShown = true;
-    }
-  }
+  // togglePlaceholder(status: boolean) {
+  //   if (this.value.length > 0 || status === false) {
+  //     this.isPlaceholderShown = false;
+  //   } else {
+  //     this.isPlaceholderShown = true;
+  //   }
+  // }
 
 }
