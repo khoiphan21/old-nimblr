359eaf8c7b2af027e0dd5b3274c2a5dc7759000f
Jeremy Yung Hau Ng <jeremy.ng1002@gmail.com>
2019-03-27 16:01:53 +1000

    create functions to render and update in mobile question block

--- src/app/components/block/mobile-question-block/mobile-question-block.component.spec.ts
+++ src/app/components/block/mobile-question-block/mobile-question-block.component.spec.ts
@@ -1,13 +1,30 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MobileQuestionBlockComponent } from './mobile-question-block.component';
 import { QuestionOptionComponent } from '../question-block/question-option/question-option.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { configureTestSuite } from 'ng-bullet';
-
+import { QuestionType, BlockType } from 'src/API';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BlockFactoryService } from 'src/app/services/block/factory/block-factory.service';
+import { QuestionBlock } from 'src/app/classes/question-block';
+const uuidv4 = require('uuid/v4');
 describe('MobileQuestionBlockComponent', () => {
   let component: MobileQuestionBlockComponent;
   let fixture: ComponentFixture<MobileQuestionBlockComponent>;
+  let blockFactoryService: BlockFactoryService;
+  const rawData = {
+    id: uuidv4(),
+    type: BlockType.QUESTION,
+    version: uuidv4(),
+    documentId: uuidv4(),
+    lastUpdatedBy: uuidv4(),
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+    question: 'Is this a test value?',
+    answers: [''],
+    questionType: QuestionType.PARAGRAPH,
+  };
   configureTestSuite(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -16,15 +33,20 @@ describe('MobileQuestionBlockComponent', () => {
       ],
       imports: [
         FormsModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        BrowserAnimationsModule
       ]
     });
   });
 
   beforeEach(() => {
+    blockFactoryService = TestBed.get(BlockFactoryService);
+    const block = blockFactoryService.createAppBlock(rawData);
     fixture = TestBed.createComponent(MobileQuestionBlockComponent);
     component = fixture.componentInstance;
     spyOn(component, 'toggleOptions').and.callThrough();
+    component.questionBlock = block as QuestionBlock;
+    component.ngOnChanges();
     fixture.detectChanges();
   });
 
@@ -47,13 +69,53 @@ describe('MobileQuestionBlockComponent', () => {
 
   describe('selectType()', () => {
     it('should change the `currentType` to the right value', () => {
-      component.selectType('multiple choice');
-      expect(component.currentType).toBe('multiple choice');
+      component.selectType(QuestionType.MULTIPLE_CHOICE);
+      expect(component.currentType).toBe(QuestionType.MULTIPLE_CHOICE);
     });
 
     it('should toggle the option', () => {
-      component.selectType('multiple choice');
+      component.selectType(QuestionType.MULTIPLE_CHOICE);
       expect(component.toggleOptions).toHaveBeenCalled();
+    });
+  });
+
+  /* tslint:disable:no-string-literal */
+  describe('updateQuestionValue', () => {
+    let blockCommandSpy: jasmine.Spy;
+    const emittedValue = {
+      answers: [],
+    };
+    beforeEach(() => {
+      // setup the spies
+      spyOn(component['blockFactoryService'], 'createAppBlock').and.callThrough();
+      blockCommandSpy = spyOn(component['blockCommandService'], 'updateBlock');
+      blockCommandSpy.and.returnValues(Promise.resolve());
+      component.currentType = QuestionType.PARAGRAPH;
+    });
+
+    it('should call `createAppBlock` with the right argument', async () => {
+      await component.updateQuestionValue(emittedValue);
+      expect(component['blockFactoryService'].createAppBlock).toHaveBeenCalledWith({
+        id: component.questionBlock.id,
+        type: component.questionBlock.type,
+        documentId: component.questionBlock.documentId,
+        // createdAt: component.block.createdAt
+        lastUpdatedBy: component.questionBlock.lastUpdatedBy,
+        question: component.questionBlock.question,
+        answers: [],
+        questionType: component.questionBlock.questionType,
+        options: undefined
+      });
+    });
+
+    it('should resolve with the updated block', async () => {
+      const updatedBlock: QuestionBlock = await component.updateQuestionValue(emittedValue) as QuestionBlock;
+      expect(updatedBlock.question).toEqual(component.questionBlock.question);
+    });
+
+    it('should call block command service with the right argument', async () => {
+      const updatedBlock = await component.updateQuestionValue(emittedValue);
+      expect(blockCommandSpy).toHaveBeenCalledWith(updatedBlock);
     });
   });
 });
--- src/app/components/block/mobile-question-block/mobile-question-block.component.ts
+++ src/app/components/block/mobile-question-block/mobile-question-block.component.ts
@@ -1,37 +1,68 @@
-import { Component, OnInit } from '@angular/core';
-import { slideBottomToTopAnimation } from 'src/app/animation';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import { slideBottomToTopAnimationDOM } from 'src/app/animation';
+import { QuestionBlock } from 'src/app/classes/question-block';
+import { QuestionType } from 'src/API';
+import { Block } from 'src/app/classes/block';
+import { BlockCommandService } from 'src/app/services/block/command/block-command.service';
+import { BlockFactoryService } from 'src/app/services/block/factory/block-factory.service';
 
 @Component({
   selector: 'app-mobile-question-block',
   templateUrl: './mobile-question-block.component.html',
   styleUrls: ['./mobile-question-block.component.scss'],
-  animations: [slideBottomToTopAnimation]
+  animations: [slideBottomToTopAnimationDOM]
 })
-export class MobileQuestionBlockComponent implements OnInit {
+export class MobileQuestionBlockComponent implements OnChanges {
+  @Input() questionBlock: QuestionBlock;
   isPreviewMode = true;
   isQuestionOptionShown = false;
-  question = 'Untitled Question';
-  currentType = 'checkbox';
-  data = {
-    options: [
-      {
-        checked: false,
-        answer: ''
-      }
-    ]
-  };
-  constructor() { }
+  previewAnswers = [];
+  previewOptions = [];
+  answers = [];
+  options = [];
+  question = '';
+  currentType: QuestionType;
+  constructor(
+    private blockFactoryService: BlockFactoryService,
+    private blockCommandService: BlockCommandService
+  ) { }
 
-  ngOnInit() {
+  ngOnChanges() {
+    this.answers = this.questionBlock.answers;
+    this.options = this.questionBlock.options;
+    this.previewAnswers = this.questionBlock.answers;
+    this.previewOptions = this.questionBlock.options;
+    this.question = this.questionBlock.question;
+    this.currentType = this.questionBlock.questionType;
   }
 
   toggleOptions() {
     this.isQuestionOptionShown = this.isQuestionOptionShown ? false : true;
   }
 
-  selectType(type: string) {
+  selectType(type: QuestionType) {
     this.currentType = type;
     this.toggleOptions();
   }
 
+  updateQuestionValue(event: any): Promise<Block> {
+    this.previewAnswers = event.answers;
+    this.previewOptions = event.options;
+    return new Promise(resolve => {
+      const updatedBlock: Block = this.blockFactoryService.createAppBlock({
+        id: this.questionBlock.id,
+        type: this.questionBlock.type,
+        documentId: this.questionBlock.documentId,
+        lastUpdatedBy: this.questionBlock.lastUpdatedBy,
+        // createdAt: this.block.createdAt
+        question: this.question,
+        answers: event.answers,
+        questionType: this.currentType,
+        options: event.options
+      });
+      this.blockCommandService.updateBlock(updatedBlock).then(() => {
+        resolve(updatedBlock);
+      });
+    });
+  }
 }
