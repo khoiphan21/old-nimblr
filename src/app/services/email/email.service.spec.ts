94f4fc964e9d23fea3325734018dbd0d019ba7b4
Khoi Phan <khoiphan21@gmail.com>
2019-04-11 23:18:56 +1000

    add tests for EmailService

--- src/app/services/email/email.service.spec.ts
+++ src/app/services/email/email.service.spec.ts
@@ -2,6 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { EmailService } from './email.service';
 
+// tslint:disable:no-string-literal
 describe('EmailService', () => {
   let service: EmailService;
   beforeEach(() => {
@@ -11,6 +12,60 @@ describe('EmailService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('sendInvitationEmail()', () => {
+    it('should call send()', async () => {
+      spyOn<any>(service, 'send');
+      service.sendInvitationEmail({
+        email: 'test',
+        documentId: 'abcd',
+        sender: null
+      });
+      expect(service['send']).toHaveBeenCalled();
+    });
+  });
+
+  describe('send()', () => {
+    let ses: any;
+    let sesSpy: jasmine.Spy;
+    let params: any;
+
+    beforeEach(() => {
+      ses = {
+        sendEmail: () => { }
+      };
+      params = { test: 'value' };
+      sesSpy = spyOn(ses, 'sendEmail');
+    });
+
+    it('should call sendEmail of the given service with the right params', async () => {
+      service['send'](ses, params);
+      expect(sesSpy.calls.mostRecent().args[0]).toEqual(params);
+    });
+
+    it('should resolve with the data given', async () => {
+      const data = { test: 'value2' };
+      sesSpy.and.callFake((_, callback) => {
+        callback(null, data);
+      });
+      const value = await service['send'](ses, params);
+      expect(value).toEqual(data);
+    });
+
+    it('should reject with the error given', async () => {
+      const error = new Error('test');
+      sesSpy.and.callFake((_, callback) => {
+        callback(error, null);
+      });
+      try {
+        await service['send'](ses, params);
+        fail('error must occur');
+      } catch (thrownError) {
+        expect(thrownError).toEqual(error);
+      }
+    });
+
   });
 
 });
