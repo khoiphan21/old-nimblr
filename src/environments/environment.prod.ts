bc46391aba1f22a3f5b51e4a2a3f5e396434f19b
Khoi Phan <khoiphan21@gmail.com>
2019-04-17 12:07:50 +1000

    update the prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  AWS_ACCESS_KEY_ID: 'AKIAIU5JQXYYOIHRYZTQ',
+  AWS_SECRET_ACCESS_KEY: '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',
 };
