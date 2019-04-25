e1f39c48d050c04d5895fe3416af05a3b75fb620
Khoi Phan <khoiphan21@gmail.com>
2019-04-25 14:44:43 +1000

    refactor registerPage

--- src/app/pages/register-page/register-page.component.ts
+++ src/app/pages/register-page/register-page.component.ts
@@ -94,7 +94,7 @@ export class RegisterPageComponent implements OnInit {
     }
   }
 
-  registerAccountInAws(): Promise<any> {
+  async registerAccountInAws() {
     const email = this.registerForm.get('email').value;
     const firstName = this.registerForm.get('firstName').value;
     const lastName = this.registerForm.get('lastName').value;
@@ -109,52 +109,42 @@ export class RegisterPageComponent implements OnInit {
         family_name: `${lastName}`
       }
     };
-    return this.accountService.registerCognitoUser(this.newCognitoUser).then((data) => {
-      this.uuid = data.userSub;
-      this.steps = 'three';
-      return Promise.resolve();
-    });
+    const data = await this.accountService.registerCognitoUser(this.newCognitoUser);
+    this.uuid = data.userSub;
+    this.steps = 'three';
   }
 
-  verifyAccount(): Promise<any> {
+  async verifyAccount(): Promise<any> {
     const verificationcode = this.verificationForm.get('verificationCode').value;
     const email = this.newCognitoUser.username;
-    return this.accountService.awsConfirmAccount(email, verificationcode).then(() => {
-      this.createAccountInDatabase();
-      return Promise.resolve();
-    }).catch(error => {
-      return Promise.reject();
-    });
+
+    await this.accountService.awsConfirmAccount(email, verificationcode);
+
+    await this.createAccountInDatabase();
   }
 
-  async createAccountInDatabase(): Promise<any> {
-    try {
-      if (this.newCognitoUser.attributes === null) {
-        this.getCognitoUserDetails();
-      } else {
-        await this.accountService.registerAppUser(this.newCognitoUser, this.uuid);
-        this.router.navigate([`/dashboard`]);
-        return Promise.resolve();
-      }
-    } catch (error) {
-      return Promise.reject(error);
+  async createAccountInDatabase() {
+    if (this.newCognitoUser.attributes === null) {
+      await this.getCognitoUserDetails();
+    } else {
+      await this.accountService.registerAppUser(this.newCognitoUser, this.uuid);
+
+      this.router.navigate([`/dashboard`]);
     }
   }
 
-  getCognitoUserDetails(): Promise<any> {
+  async getCognitoUserDetails() {
     const username = this.newCognitoUser.username;
     const password = this.newCognitoUser.password;
-    return Auth.signIn(username, password).then(() => {
-      return Auth.currentAuthenticatedUser({ bypassCache: false });
-    }).catch(error => {
-      return Promise.reject();
-    }).then(user => {
-      const details = user.attributes;
-      this.setNewCognitoUser(details);
-      return this.createAccountInDatabase();
-    }).catch(error => {
-      return Promise.reject();
-    });
+
+    await Auth.signIn(username, password);
+
+    const user = await Auth.currentAuthenticatedUser({ bypassCache: false });
+
+    const details = user.attributes;
+    this.setNewCognitoUser(details);
+
+    await this.createAccountInDatabase();
   }
 
   private setNewCognitoUser(value: any) {
