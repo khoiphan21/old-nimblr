6e9e3e7f7b0380bd624b0dfdd9a44ffb667e3863
Jeremy Yung Hau Ng <jeremy.ng1002@gmail.com>
2019-05-05 12:36:44 +1000

    fix failed tests in question option component

--- src/app/components/block/question-block/question-block.component.ts
+++ src/app/components/block/question-block/question-block.component.ts
@@ -90,7 +90,7 @@ export class QuestionBlockComponent implements OnInit, OnChanges {
         documentId: this.questionBlock.documentId,
         lastUpdatedBy: this.questionBlock.lastUpdatedBy,
         createdAt: this.questionBlock.createdAt,
-        question: 'deleted',
+        question: this.question,
         answers: event.answers,
         questionType: this.currentType,
         options: event.options
